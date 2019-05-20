6f12cb23cc4f6cbfad704056ec98eb7950040521
hunorkonczey <hunorkonczey@gmail.com>
2019-05-21 00:29:00 +0300

    Menu

--- App/app/src/main/java/com/example/qrcodescanner/Activities/MainActivity.kt
+++ App/app/src/main/java/com/example/qrcodescanner/Activities/MainActivity.kt
@@ -1,33 +1,119 @@
 package com.example.qrcodescanner.Activities
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Toast
 import com.example.qrcodescanner.R
 import com.google.zxing.integration.android.IntentIntegrator
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener
+{
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_scan.setOnClickListener {
-            val scanner = IntentIntegrator(this)
 
-            scanner.setBeepEnabled(false)
-            scanner.initiateScan()
+
+        loadNavBar()
+
+    }
+
+    private fun loadNavBar()
+    {
+        val toolbar: Toolbar                = findViewById(R.id.toolbar_main)
+        val navigationView: NavigationView  = findViewById( R.id.nav_view )
+
+        navigationView.setNavigationItemSelectedListener( this )
+        setSupportActionBar( toolbar )
+
+        drawer = findViewById( R.id.drawer_layout )
+        toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+
+        drawer.addDrawerListener( toggle )
+        supportActionBar?.setDisplayHomeAsUpEnabled( true )
+        supportActionBar?.setHomeButtonEnabled( true )
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?)
+    {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?)
+    {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean
+    {
+        if (toggle.onOptionsItemSelected(item))
+        {
+            return true
         }
 
-        button_connection.setOnClickListener {
-            val intent = Intent( this, SubjectActivity::class.java )
+        return super.onOptionsItemSelected(item)
+    }
 
-            startActivity( intent )
+    override fun onNavigationItemSelected( item: MenuItem ): Boolean
+    {
+        when ( item.itemId )
+        {
+            R.id.qr_code ->
+            {
+               startScan()
+            }
+            R.id.subject ->
+            {
+                startSubjectActivity()
+            }
         }
+
+        drawer.closeDrawer( GravityCompat.START )
+
+        return true
+    }
+
+    override fun onBackPressed()
+    {
+        if ( drawer.isDrawerOpen( GravityCompat.START ) )
+        {
+            drawer.closeDrawer( GravityCompat.START )
+        }
+        else
+        {
+            super.onBackPressed()
+        }
+    }
+
+    private fun startScan()
+    {
+        val scanner = IntentIntegrator(this)
+
+        scanner.setBeepEnabled(false)
+        scanner.initiateScan()
+    }
+
+    private fun startSubjectActivity()
+    {
+        val intent = Intent( this, SubjectActivity::class.java )
+
+        startActivity( intent )
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -44,7 +130,7 @@ class MainActivity : AppCompatActivity() {
             else
             {
                 Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
-                link.text = result.contents
+                //link.text = result.contents
             }
         }
         else
