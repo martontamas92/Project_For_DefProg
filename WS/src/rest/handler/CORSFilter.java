59ee57587913293cb6e0932cdab0948019417b5e
nor_bika92@yahoo.com <nor_bika92@yahoo.com>
2019-06-01 20:11:30 +0300

    CORSFilter changes

--- WS/src/rest/handler/CORSFilter.java
+++ WS/src/rest/handler/CORSFilter.java
@@ -19,11 +19,6 @@ public class CORSFilter implements ContainerResponseFilter {
         response.getHeaders().add("Access-Control-Allow-Methods",
                 "GET, POST, PUT, DELETE, OPTIONS, HEAD");
 
-        request.getHeaders().add("Access-Control-Allow-Origin", "*");
-        request.getHeaders().add("Access-Control-Allow-Headers",
-                "origin, content-type, accept, authorization");
-        request.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        request.getHeaders().add("Access-Control-Allow-Methods",
-                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+
     }
 }
