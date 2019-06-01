16c4dc5825f10fc2f6dd9bc53f3b2f4636b956c0
martontamas92 <martontamas92@gmail.com>
2019-06-01 17:24:33 +0300

    asd

--- WS/src/rest/handler/ApplicationConfig.java
+++ WS/src/rest/handler/ApplicationConfig.java
@@ -3,13 +3,7 @@ package rest.handler;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-import org.glassfish.jersey.server.ResourceConfig;
-
-
-
 @ApplicationPath("/")
-public class ApplicationConfig extends ResourceConfig {
-	public ApplicationConfig() {
-	       register( new CORSFilter() );
-	   }
+public class ApplicationConfig extends Application {
+
 }
